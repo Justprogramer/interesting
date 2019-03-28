3be8a6e972a8d44edc2b1a5b36006a6b73999d80
taopeng <peng.tao@redteadata.com>
2019-03-28 16:48:08 +0800

    剑指offer: 判断是否是二叉平衡树

---
+++ src/main/java/com/tao/swordoffer/CheckBalanceTree.java
@@ -0,0 +1,31 @@
+package com.tao.swordoffer;
+
+import java.util.Optional;
+
+/**
+ * @author: Penger
+ * @time: 2019/3/28
+ * @description: <p>判断一棵树是否是二叉平衡树
+ * </p>
+ * @solution: 使用递归判断左右子树是否是二叉平衡树，已经左右子树的高度相差小于1
+ **/
+public class CheckBalanceTree {
+
+    public boolean isBalanceTree(TreeNode root) {
+        return Optional.ofNullable(root).map(r -> {
+            int left = treeDepth(r.left);
+            int right = treeDepth(r.right);
+            return Math.abs(left - right) <= 1
+                    && isBalanceTree(r.left)
+                    && isBalanceTree(r.right);
+        }).orElse(true);
+    }
+
+    private int treeDepth(TreeNode root) {
+        return Optional.ofNullable(root).map(r -> {
+            int left = treeDepth(r.left);
+            int right = treeDepth(r.right);
+            return Math.max(left, right) + 1;
+        }).orElse(0);
+    }
+}
