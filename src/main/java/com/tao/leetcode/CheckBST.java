ac543b8150c9afc2cee2724fce2339dccbeeefe5
Penger <whupenger@gmail.com>
2019-04-01 16:47:57 +0800

    添加另外几种方法判断二叉排序树

--- src/main/java/com/tao/leetcode/CheckBST.java
+++ src/main/java/com/tao/leetcode/CheckBST.java
@@ -1,6 +1,7 @@
 package com.tao.leetcode;
 
 import com.tao.common.TreeNode;
+import org.junit.Test;
 
 import java.util.Optional;
 
@@ -16,7 +17,7 @@ import java.util.Optional;
  * </p>
  **/
 public class CheckBST {
-    private  boolean isValidBST(TreeNode root){
+    private boolean isValidBST(TreeNode root) {
         return Optional.ofNullable(root).map(node -> {
             TreeNode left = node.left;
             // node 大于 左节点的最大值
@@ -36,5 +37,49 @@ public class CheckBST {
             }
             return isValidBST(node.left) && isValidBST(node.right);
         }).orElse(true);
+    }
+
+    private boolean isValidBSTTwo(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+
+    private boolean isValidBST(TreeNode current, TreeNode min, TreeNode max) {
+        if (current == null) {
+            return true;
+        }
+        if (min != null && current.val <= min.val) {
+            return false;
+        }
+        if (max != null && current.val >= max.val) {
+            return false;
+        }
+        return isValidBST(current.left, min, current) && isValidBST(current.right, current, max);
+    }
+
+    private boolean isValidBSTThree(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        return root == null
+                || root.val < maxVal && root.val > minVal
+                && isValidBST(root.left, minVal, root.val)
+                && isValidBST(root.right, root.val, maxVal);
+    }
+
+    @Test
+    public void test() {
+        TreeNode root = new TreeNode(10);
+        TreeNode rl = new TreeNode(5);
+        TreeNode rr = new TreeNode(15);
+        TreeNode rrl = new TreeNode(6);
+        TreeNode rrr = new TreeNode(20);
+        root.left = rl;
+        root.right = rr;
+        rr.left = rrl;
+        rr.right = rrr;
+        System.out.println(isValidBST(root));
+        System.out.println(isValidBSTTwo(root));
+        System.out.println(isValidBSTThree(root));
     }
 }
